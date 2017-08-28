aa51a8e41657732f80d590e27422f8237231feed
Tora1024 <heavenhell64@gmail.com>
2017-08-28 13:57:47 -0600

    Adding dev-tools into the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
@@ -12,7 +13,9 @@ import { createBrowserHistory } from 'history';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 const App = () => {
-  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+  const store = createStore(reducers, {}, composeWithDevTools(
+		applyMiddleware(ReduxThunk),
+	));
   const history = syncHistoryWithStore(createBrowserHistory(), store);
 
   return (
