d9ca5c91ac612f0311981c81054aeaa52c7f9e6f
Tora1024 <heavenhell64@gmail.com>
2017-08-30 22:06:28 -0600

    Needed in order to use redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
+import { reducer as form } from 'redux-form';
 
 import PostsReducer from './reducerPosts/reducerPosts';
 
 export default combineReducers({
 	posts: PostsReducer,
 	routing,
+	form,
 });
