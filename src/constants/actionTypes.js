93a5090a199040022adda13612439fa6d349addf
Tora1024 <heavenhell64@gmail.com>
2017-08-30 22:04:14 -0600

    Adding more action creators

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,4 +2,10 @@ export default {
   fetch_posts_Request: 'FETCH_POSTS_REQUEST',
   fetch_posts_Success: 'FETCH_POSTS_SUCCESS',
   fetch_posts_Failure: 'FETCH_POSTS_FAILURE',
+  create_posts_Request: 'CREATE_POSTS_REQUEST',
+  create_posts_Success: 'CREATE_POSTS_SUCCESS',
+  create_posts_Failure: 'CREATE_POSTS_FAILURE',
+  delete_posts_Request: 'DELETE_POSTS_REQUEST',
+  delete_posts_Success: 'DELETE_POSTS_SUCCESS',
+  delete_posts_Failure: 'DELETE_POSTS_FAILURE',
 };
