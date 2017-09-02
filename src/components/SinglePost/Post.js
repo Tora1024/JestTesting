4f491ac43d5e41f9f60de10faa0d7975e417739f
Tora1024 <heavenhell64@gmail.com>
2017-09-02 13:28:45 -0600

    Added an options props, to reuse the same component on both the posts index and the detailed view

--- src/components/SinglePost/Post.js
+++ src/components/SinglePost/Post.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CSSModules from 'react-css-modules';
 import PropTypes from 'prop-types';
 import styles from './post.css';
 
-const Post = ({post}) => {
-  return (
-    <main styleName='container'>
-      <h3 styleName='title'>{post.title}</h3>
-      <section styleName='categories'>{post.categories}</section>
-      <article styleName='content'>{post.content}</article>
-    </main>
-  );
+const Post = ({post, options}) => {
+	if (options === 'simple') {
+		return (
+			<main styleName='container-simple'>
+				<h3 styleName='title'>{post.title}</h3>
+				<Link styleName='nav-post' to={`/${post.id}`}>Go to this Post</Link>
+			</main>
+		);
+	}	else if ( options === 'detailed' ) {
+		return (
+			<main styleName='container-detailed'>
+				<h3 styleName='title'>{post.title}</h3>
+				<section styleName='categories'>{post.categories}</section>
+				<article styleName='content'>{post.content}</article>
+			</main>
+		);
+	}
 };
 
 Post.propTypes = {
   post: PropTypes.object,
+  options: PropTypes.string,
 }
 
 export default CSSModules(Post, styles);
