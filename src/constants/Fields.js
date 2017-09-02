b354e09396ec374697b6ff20993c6130857acdd6
Tora1024 <heavenhell64@gmail.com>
2017-09-02 13:33:22 -0600

    A new constants file to track all fields from forms

---
+++ src/constants/Fields.js
@@ -0,0 +1,19 @@
+const FIELDS = {
+	title: {
+		type: 'input',
+		label: 'Title',
+		error_msg: 'Enter a title',
+	},
+	categories: {
+		type: 'input',
+		label: 'Categories',
+		error_msg: 'Enter some categories',
+	},
+	content: {
+		type: 'textarea',
+		label: 'Content',
+		error_msg: 'Enter some content',
+	},
+};
+
+export default FIELDS;
