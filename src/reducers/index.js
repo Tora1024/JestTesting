cc9a2846719b7a24a1bcf914e581c66f25d15152
Tora1024 <heavenhell64@gmail.com>
2017-09-06 08:53:59 -0600

    Registering the PostsSelectedReducer into the combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { routerReducer as routing } from 'react-router-redux';
 import { reducer as form } from 'redux-form';
 
 import PostsReducer from './reducerPosts/reducerPosts';
+import PostsSelectedReducer from './reducerPosts/reducerSelectedPosts';
 
 export default combineReducers({
 	posts: PostsReducer,
+	selected_posts: PostsSelectedReducer,
 	routing,
 	form,
 });
